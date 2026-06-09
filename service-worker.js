447b4117a692c0e84bdafc7db4d1f20d92cae267
solourban <kokiri2525@gmail.com>
2026-06-09 17:22:40 +0900

    fix: complete service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,5 @@
-const CACHE_NAME='golabang-v1';
-const CORE_ASSETS=['./','./index.html','./manifest.json','./icons/icon.svg','./icons/maskable-icon.svg'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
-});
-
-self.addEventListener('fetch',event=>{
+const CACHE='golabang-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon.svg','./icons/maskable-icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new
